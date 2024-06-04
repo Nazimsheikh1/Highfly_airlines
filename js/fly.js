e2ca3cbbce45cd62a4df5a8faed35cc9433738f2
Nazim Sheikh <135043882+Nazimsheikh1@users.noreply.github.com>
2024-06-04 15:43:27 +0530

    Add files via upload

---
+++ js/fly.js
@@ -0,0 +1,4 @@
+document.getElementById('flight-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+});
---
+++ js/spin.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.stat-number');
+    const speed = 200; // The lower the number, the faster the count
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+
+            // Lower inc to slow and higher to slow
+            const inc = target / speed;
+
+            // Check if target is reached
+            if (count < target) {
+                // Add inc to count and output in counter
+                counter.innerText = Math.ceil(count + inc);
+                // Call function every ms
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCount();
+    });
+});
